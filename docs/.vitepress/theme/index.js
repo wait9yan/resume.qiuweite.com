72a0408e3a7e41a2822c5d75a531abf539357479
wait9Yan <wait9yan@qq.com>
2024-10-16 22:44:20 +0800

    fix: update deploy

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,15 +2,15 @@
  * @Author       : wait9yan
  * @Date         : 2024-05-23 19:09:25
  * @LastEditors  : wait9Yan
- * @LastEditTime : 2024-10-16 16:19:31
+ * @LastEditTime : 2024-10-16 16:55:37
  * @FilePath     : \resume.qiuweite.com\docs\.vitepress\theme\index.js
  * @Description  : 
  */
 // https://vitepress.dev/guide/custom-theme
 import '@fortawesome/fontawesome-free/css/all.css'
-import './fontawesome.css'
 import Layout from './Layout.vue'
 import './style.css'
+import './fontawesome.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
